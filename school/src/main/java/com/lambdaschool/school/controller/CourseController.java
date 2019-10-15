ac60227ef341bcc43f7ead97587f5035af126a52
Diana Grams <deegrams221@gmail.com>
2019-10-15 14:55:42 -0600

    Initial project setup and started on adding custom Swagger documentation

--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -1,8 +1,9 @@
 package com.lambdaschool.school.controller;
 
 import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.ErrorDetail;
 import com.lambdaschool.school.service.CourseService;
-import com.lambdaschool.school.view.CountStudentsInCourses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,20 @@ public class CourseController
     @Autowired
     private CourseService courseService;
 
+    // Adding custom swagger documentation for list all courses
+    @ApiOperation(value = "Return all Courses", response = Course.class,
+            responseContainer = "List")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string",
+                    paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+                            "Multiple sort criteria are supported.")})
+
     @GetMapping(value = "/courses", produces = {"application/json"})
     public ResponseEntity<?> listAllCourses()
     {
@@ -24,14 +39,40 @@ public class CourseController
         return new ResponseEntity<>(myCourses, HttpStatus.OK);
     }
 
+    // Adding custom swagger documentation for Count of Students in Courses
+    @ApiOperation(value = "Return a Count of Students in Courses",
+            response = Course.class,
+            responseContainer = "List")
+    @ApiResponses(value = {@ApiResponse(code = 200,
+            message = "Count Found",
+            response = Course.class),
+            @ApiResponse(code = 404,
+                    message = "Count Not Found",
+                    response = ErrorDetail.class)})
+
     @GetMapping(value = "/studcount", produces = {"application/json"})
     public ResponseEntity<?> getCountStudentsInCourses()
     {
         return new ResponseEntity<>(courseService.getCountStudentsInCourse(), HttpStatus.OK);
     }
 
+    // Adding custom swagger documentation for delete course by id
+    @ApiOperation(value = "Delete a course",
+            response = void.class)
+    @ApiResponses(value = {@ApiResponse(code = 200,
+            message = "Course Deleted",
+            response = void.class),
+            @ApiResponse(code = 500,
+                    message = "Could Not Delete Course",
+                    response = ErrorDetail.class)})
+
     @DeleteMapping("/courses/{courseid}")
-    public ResponseEntity<?> deleteCourseById(@PathVariable long courseid)
+    public ResponseEntity<?> deleteCourseById(
+            // Adding custom swagger params for delete course
+            @ApiParam(value = "Course Id",
+                    required = true,
+                    example = "1")
+            @PathVariable long courseid)
     {
         courseService.delete(courseid);
         return new ResponseEntity<>(HttpStatus.OK);
